009f7807c3ba86dc5d48f7a7070dfedea0c3552d
mishrabhi <asmyselfabhishek00@gmail.com>
2022-04-16 15:01:53 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log('hello world');
+console.log(4);
+console.log(4+36);
+console.log(true);
+console.log([2,5,7,99]);
+console.warn('This is a warning');
+console.timeEnd('Your code took');
---
+++ js/prob.js
@@ -0,0 +1,7 @@
+var a = 10;
+function foo(){
+    console.log(a)
+    var a = 20;
+    console.log(a)
+}
+foo()
